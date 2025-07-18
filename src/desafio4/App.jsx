d05ae9695e11b9ab9c63f35283eeb6863b0c382a
kauan <kauancavalcante8p@gmail.com>
2025-07-17 22:49:12 -0300

    adiocionando desafio 4

---
+++ src/desafio4/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+export default function Desafio4app() {
+  const [name, setName] = useState('');
+
+  function handleClick() {
+    setName(prompt('What is your name?'));
+    alert(`Hello, ${name}!`);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Greet
+    </button>
+  );
+}
